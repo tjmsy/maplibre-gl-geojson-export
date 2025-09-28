d6ddde8f7de135e5b73a126f8c422bce5063db86
tjmsy <18753632+tjmsy@users.noreply.github.com>
2025-09-29 03:01:01 +0900

    bug fix

--- docs/main.js
+++ docs/main.js
@@ -1,12 +1,13 @@
 import GeoJsonExportControl from "https://cdn.jsdelivr.net/gh/tjmsy/maplibre-gl-geojson-export/src/maplibre-gl-geojson-export.js";
 
-
 const map = new maplibregl.Map({
-  container: 'map',
-  style: 'https://tiles.openfreemap.org/styles/liberty',
+  container: "map",
+  style: "https://tiles.openfreemap.org/styles/liberty",
   center: [0, 0],
   zoom: 1,
   hash: true,
 });
 
-map.addControl(new GeoJsonExportControl(), 'top-left');
+map.on("load", () => {
+  map.addControl(new GeoJsonExportControl(), "top-left");
+});
